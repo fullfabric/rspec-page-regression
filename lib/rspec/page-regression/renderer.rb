ecc339f39573f7add1a4a55f4eb3e2ee12ad9805
Miguel Guinada <mguinada@gmail.com>
2015-10-28 21:02:48 +0000

    Don't resize the browser

--- lib/rspec/page-regression/renderer.rb
+++ lib/rspec/page-regression/renderer.rb
@@ -2,14 +2,15 @@ module RSpec::PageRegression
   module Renderer
 
     def self.render(page, test_image_path)
-
       test_image_path.dirname.mkpath unless test_image_path.dirname.exist?
+
       # Capybara doesn't implement resize in API
-      unless page.driver.respond_to? :resize
-        page.driver.browser.manage.window.resize_to *RSpec::PageRegression.page_size
-      else 
-        page.driver.resize *RSpec::PageRegression.page_size
-      end
+      # unless page.driver.respond_to? :resize
+      #   page.driver.browser.manage.window.resize_to *RSpec::PageRegression.page_size
+      # else
+      #   page.driver.resize *RSpec::PageRegression.page_size
+      # end
+
       page.driver.save_screenshot test_image_path, :full => true
     end
   end
