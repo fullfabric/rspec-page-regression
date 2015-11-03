df4984ea8873c50eb8b43e9189a0fdcf60d4d962
Miguel Guinada <mguinada@gmail.com>
2015-11-03 18:44:50 +0000

    Bind the screenshot size

--- lib/rspec/page-regression/renderer.rb
+++ lib/rspec/page-regression/renderer.rb
@@ -4,14 +4,10 @@ module RSpec::PageRegression
     def self.render(page, test_image_path)
       test_image_path.dirname.mkpath unless test_image_path.dirname.exist?
 
-      # Capybara doesn't implement resize in API
-      # unless page.driver.respond_to? :resize
-      #   page.driver.browser.manage.window.resize_to *RSpec::PageRegression.page_size
-      # else
-      #   page.driver.resize *RSpec::PageRegression.page_size
-      # end
+      width  = Capybara.current_session.driver.evaluate_script("window.innerWidth")
+      height = Capybara.current_session.driver.evaluate_script("window.innerHeight")
 
-      page.driver.save_screenshot test_image_path, :full => true
+      page.driver.save_screenshot test_image_path, width: width, height: height
     end
   end
 end
